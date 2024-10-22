03453cdbf08f8d32e37d8414101f636ee3d9ef91
nk-randers <nivethan.kandiah@randers.dk>
2024-10-22 11:32:33 +0200

    Update DatabaseClient
    using sqlalchemy session

--- python/src/utils/database.py
+++ python/src/utils/database.py
@@ -1,6 +1,6 @@
 import sqlalchemy
 import logging
-
+from sqlalchemy.orm import sessionmaker, scoped_session
 
 class DatabaseClient:
     def __init__(self, db_type, database, username, password, host, port=None):
@@ -11,7 +11,7 @@ class DatabaseClient:
         elif db_type.lower() == 'postgresql':
             driver = 'postgresql+psycopg2'
         else:
-            raise ValueError(f"Invalid database type {type}")
+            raise ValueError(f"Invalid database type {db_type}")
 
         self.logger = logging.getLogger(__name__)
 
@@ -19,18 +19,36 @@ class DatabaseClient:
             host = host + f':{port}'
 
         self.engine = sqlalchemy.create_engine(f'{driver}://{username}:{password}@{host}/{database}')
+        self.Session = scoped_session(sessionmaker(bind=self.engine))
+        self.logger.debug(f"Database engine created with driver {driver}")
 
-    def get_connection(self):
+    def get_session(self):
         try:
-            if self.engine:
-                return self.engine.connect()
-            self.logger.error("DatabaseClient not initialized properly. Engine is None. Check error from init.")
+            self.logger.debug("Creating a new database session...")
+            return self.Session()
         except Exception as e:
-            self.logger.error(f"Error connecting to database: {e}")
+            self.logger.error(f"Error creating session: {e}")
 
-    def execute_sql(self, sql):
+    def close_session(self, session):
         try:
-            with self.get_connection() as conn:
-                return conn.execute(sqlalchemy.text(sql))
+            self.logger.debug("Closing the database session...")
+            session.close()
+        except Exception as e:
+            self.logger.error(f"Error closing session: {e}")
+
+    def execute_sql(self, sql, params=None):
+        session = self.get_session()
+        try:
+            if params:
+                self.logger.debug(f"Executing SQL with params: {sql} {params}")
+                result = session.execute(sqlalchemy.text(sql), params)
+            else:
+                self.logger.debug(f"Executing SQL: {sql}")
+                result = session.execute(sqlalchemy.text(sql))
+            session.commit()
+            return result
         except Exception as e:
             self.logger.error(f"Error executing SQL: {e}")
+            session.rollback()
+        finally:
+            self.close_session(session)
