3280bbffacc107e898a12c6b1ca7be72fc5890c5
nk-randers <nivethan.kandiah@randers.dk>
2024-10-23 14:34:04 +0200

    Updated Opgaver model

--- python/src/models/opgaver.py
+++ python/src/models/opgaver.py
@@ -1,18 +1,18 @@
-from sqlalchemy import Column, Integer, String, DateTime, Boolean
-from sqlalchemy.ext.declarative import declarative_base
-
-
-Base = declarative_base()
+from sqlalchemy import Column, Integer, String, DateTime, Boolean, ForeignKey
+from sqlalchemy.orm import relationship
+from models.base import Base
 
 
 class Opgaver(Base):
     __tablename__ = 'Opgaver'
     OpgaverID = Column(Integer, primary_key=True, autoincrement=True)
-    title = Column(String)
-    beskrivelse = Column(String)
-    resourcer = Column(String)
-    ansvarlig = Column(String)
-    startdato = Column(DateTime)
-    slutdato = Column(DateTime)
-    result = Column(Boolean)
-    timestamp = Column(DateTime)
+    title = Column(String, nullable=False) 
+    beskrivelse = Column(String, nullable=False)
+    resourcer = Column(String, nullable=False)
+    ansvarlig = Column(String, nullable=False)
+    startdato = Column(DateTime, nullable=False)
+    slutdato = Column(DateTime, nullable=False)
+    result = Column(Boolean, nullable=False)
+    timestamp = Column(DateTime, nullable=False)
+    ForløbsskabelonID = Column(Integer, ForeignKey('Forløbsskabelon.ForløbsskabelonID'))
+    forløbsskabelon = relationship('Forløbsskabelon', back_populates='opgaver')
