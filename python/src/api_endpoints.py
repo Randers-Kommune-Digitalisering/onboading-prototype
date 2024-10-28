ad25fb38840fe31ac0c388dd4b73442e36f2bd2c
nk-randers <nivethan.kandiah@randers.dk>
2024-10-28 15:00:11 +0100

    added endpoints for ressource
    added 5 endpoints for ressource

--- python/src/api_endpoints.py
+++ python/src/api_endpoints.py
@@ -3,14 +3,13 @@ from flask import Blueprint
 from utils.database import DatabaseClient
 from utils.config import MSSQL_USER, MSSQL_PASS, MSSQL_HOST, MSSQL_DATABASE
 from models import Base
-from controllers.opgaver_controller import (
-    create_opgaver_with_forloebsskabelon_id,
-    get_all_opgaver,
-    get_opgaver_by_forloebsskabelon_id,
-    update_opgaver,
-    delete_opgaver,
-    create_opgaver_with_forloeb_id,
-    get_opgaver_by_forloeb_id
+from controllers.opgave_controller import (
+    create_opgave_with_opgaveskabelon,
+    get_opgave_by_forloebsskabelon_id,
+    update_opgave,
+    delete_opgave,
+    get_opgave_by_forloeb_id,
+    create_opgave
 )
 from controllers.forloebsskabelon_controller import (
     create_forloebsskabelon,
@@ -23,6 +22,22 @@ from controllers.forloeb_controller import (
     create_forloeb
 )
 
+from controllers.ressource_controller import (
+    create_ressource,
+    get_ressources_by_opgaveid,
+    delete_ressource,
+    update_ressource,
+    get_ressources_by_opgaveskabelonid
+
+)
+
+from controllers.opgaveskabeloner_controller import (
+    create_opgaveskabelon,
+    get_all_opgaveskabeloner,
+    update_opgaveskabelon,
+    delete_opgaveskabelon
+)
+
 db_client = DatabaseClient('mssql', MSSQL_DATABASE, MSSQL_USER, MSSQL_PASS, MSSQL_HOST)
 Base.metadata.create_all(db_client.engine)
 
@@ -31,39 +46,34 @@ logger = logging.getLogger(__name__)
 api_endpoints = Blueprint('api', __name__, url_prefix='/api')
 
 
-@api_endpoints.route('/opgaver', methods=['GET'])
-def get_all_opgaver_endpoint():
-    return get_all_opgaver()
+@api_endpoints.route('/opgave', methods=['POST'])
+def create_opgave_endpoint():
+    return create_opgave()
 
 
-@api_endpoints.route('/opgaver/forloebsskabelon', methods=['POST'])
-def create_opgave_with_forloebsskabelon_id_endpoint():
-    return create_opgaver_with_forloebsskabelon_id()
+@api_endpoints.route('/opgave/opgaveskabelon', methods=['POST'])
+def create_opgave_with_opgaveskabelon_endpoint():
+    return create_opgave_with_opgaveskabelon()
 
 
-@api_endpoints.route('/opgaver/forloebsskabelon/<int:forlobsskabelon_id>', methods=['GET'])
-def get_opgaver_by_forloebsskabelon_id_endpoint(forlobsskabelon_id):
-    return get_opgaver_by_forloebsskabelon_id(forlobsskabelon_id)
+@api_endpoints.route('/opgave/forloebsskabelon/<int:forlobsskabelon_id>', methods=['GET'])
+def get_opgave_by_forloebsskabelon_id_endpoint(forlobsskabelon_id):
+    return get_opgave_by_forloebsskabelon_id(forlobsskabelon_id)
 
 
-@api_endpoints.route('/opgaver/<int:opgave_id>', methods=['PUT'])
-def update_opgaver_endpoint(opgave_id):
-    return update_opgaver(opgave_id)
+@api_endpoints.route('/opgave/<int:opgave_id>', methods=['PUT'])
+def update_opgave_endpoint(opgave_id):
+    return update_opgave(opgave_id)
 
 
-@api_endpoints.route('/opgaver/<int:opgave_id>', methods=['DELETE'])
-def delete_opgaver_endpoint(opgave_id):
-    return delete_opgaver(opgave_id)
+@api_endpoints.route('/opgave/<int:opgave_id>', methods=['DELETE'])
+def delete_opgave_endpoint(opgave_id):
+    return delete_opgave(opgave_id)
 
 
-@api_endpoints.route('/opgaver/forloeb/<int:forloeb_id>', methods=['GET'])
-def get_opgaver_by_forloeb_id_endpoint(forloeb_id):
-    return get_opgaver_by_forloeb_id(forloeb_id)
-
-
-@api_endpoints.route('/opgaver/forloeb', methods=['POST'])
-def create_opgaver_with_forloeb_id_endpoint():
-    return create_opgaver_with_forloeb_id()
+@api_endpoints.route('/opgave/forloeb/<int:forloeb_id>', methods=['GET'])
+def get_opgave_by_forloeb_id_endpoint(forloeb_id):
+    return get_opgave_by_forloeb_id(forloeb_id)
 
 
 @api_endpoints.route('/forloeb', methods=['POST'])
@@ -84,3 +94,48 @@ def get_all_forloebsskabeloner_endpoint():
 @api_endpoints.route('/forlobsskabelon/<int:forloebsskabelon_id>', methods=['PUT'])
 def update_forloebsskabelon_name_endpoint(forloebsskabelon_id):
     return update_forloebsskabelon_name(forloebsskabelon_id)
+
+
+@api_endpoints.route('/ressource', methods=['POST'])
+def create_ressource_endpoint():
+    return create_ressource()
+
+
+@api_endpoints.route('/ressource/opgave/<int:opgave_id>', methods=['GET'])
+def get_ressources_by_opgaveid_endpoint(opgave_id):
+    return get_ressources_by_opgaveid(opgave_id)
+
+
+@api_endpoints.route('/ressource/<int:ressource_id>', methods=['DELETE'])
+def delete_ressource_endpoint(ressource_id):
+    return delete_ressource(ressource_id)
+
+
+@api_endpoints.route('/ressource/<int:ressource_id>', methods=['PUT'])
+def update_ressource_endpoint(ressource_id):
+    return update_ressource(ressource_id)
+
+
+@api_endpoints.route('/ressource/opgaveskabelon/<int:opgaveskabelon_id>', methods=['GET'])
+def get_ressources_by_opgaveskabelonid_endpoint(opgaveskabelon_id):
+    return get_ressources_by_opgaveskabelonid(opgaveskabelon_id)
+
+
+@api_endpoints.route('/opgaveskabelon', methods=['POST'])
+def create_opgaveskabelon_endpoint():
+    return create_opgaveskabelon()
+
+
+@api_endpoints.route('/opgaveskabelon', methods=['GET'])
+def get_all_opgaveskabeloner_endpoint():
+    return get_all_opgaveskabeloner()
+
+
+@api_endpoints.route('/opgaveskabelon/<int:id>', methods=['PUT'])
+def update_opgaveskabelon_endpoint(id):
+    return update_opgaveskabelon(id)
+
+
+@api_endpoints.route('/opgaveskabelon/<int:id>', methods=['DELETE'])
+def delete_opgaveskabelon_endpoint(id):
+    return delete_opgaveskabelon(id)
---
+++ python/src/controllers/ressource_controller.py
@@ -0,0 +1,121 @@
+from flask import request, jsonify
+from datetime import datetime
+from models import Ressource, Opgave, Opgaveskabelon
+from utils.db_connection import get_db_client
+
+db_client = get_db_client()
+
+
+def create_ressource():
+    session = db_client.get_session()
+    try:
+        data = request.json
+        required_fields = ['name', 'url']
+        if not all(field in data for field in required_fields):
+            return jsonify({"error": "Missing required fields"}), 400
+
+        new_ressource = Ressource(
+            name=data['name'],
+            url=data['url']
+        )
+
+        if 'OpgaveID' in data:
+            opgave = session.query(Opgave).filter_by(OpgaveID=data['OpgaveID']).first()
+            if not opgave:
+                return jsonify({"error": "Opgave not found"}), 404
+            new_ressource.OpgaveID = opgave.OpgaveID
+        elif 'OpgaveskabelonID' in data:
+            opgaveskabelon = session.query(Opgaveskabelon).filter_by(OpgaveskabelonID=data['OpgaveskabelonID']).first()
+            if not opgaveskabelon:
+                return jsonify({"error": "Opgaveskabelon not found"}), 404
+            new_ressource.OpgaveskabelonID = opgaveskabelon.OpgaveskabelonID
+        else:
+            return jsonify({"error": "Either OpgaveID or OpgaveskabelonID is required"}), 400
+
+        session.add(new_ressource)
+        session.commit()
+        return jsonify({"message": "Ressource created successfully"}), 201
+    except Exception as e:
+        session.rollback()
+        return jsonify({"error": str(e)}), 500
+    finally:
+        session.close()
+
+
+def delete_ressource(ressource_id):
+    session = db_client.get_session()
+    try:
+        ressource = session.query(Ressource).filter_by(RessourceID=ressource_id).first()
+        if not ressource:
+            return jsonify({"error": "Ressource not found"}), 404
+
+        session.delete(ressource)
+        session.commit()
+        return jsonify({"message": "Ressource deleted successfully"}), 200
+    except Exception as e:
+        session.rollback()
+        return jsonify({"error": str(e)}), 500
+    finally:
+        session.close()
+
+
+def update_ressource(ressource_id):
+    session = db_client.get_session()
+    try:
+        ressource = session.query(Ressource).filter_by(RessourceID=ressource_id).first()
+        if not ressource:
+            return jsonify({"error": "Ressource not found"}), 404
+
+        data = request.json
+        ressource.name = data.get('name', ressource.name)
+        ressource.url = data.get('url', ressource.url)
+
+        session.commit()
+        return jsonify({"message": "Ressource updated successfully"}), 200
+    except Exception as e:
+        session.rollback()
+        return jsonify({"error": str(e)}), 500
+    finally:
+        session.close()
+
+
+def get_ressources_by_opgaveid(opgave_id):
+    session = db_client.get_session()
+    try:
+        ressources = session.query(Ressource).filter_by(OpgaveID=opgave_id).all()
+        if not ressources:
+            return jsonify({"error": "No ressources found for the specified OpgaveID"}), 404
+
+        ressource_data = [
+            {
+                'RessourceID': ressource.RessourceID,
+                'name': ressource.name,
+                'url': ressource.url
+            } for ressource in ressources
+        ]
+        return jsonify(ressource_data)
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+    finally:
+        session.close()
+
+
+def get_ressources_by_opgaveskabelonid(opgaveskabelon_id):
+    session = db_client.get_session()
+    try:
+        ressources = session.query(Ressource).filter_by(OpgaveskabelonID=opgaveskabelon_id).all()
+        if not ressources:
+            return jsonify({"error": "No ressources found for the specified OpgaveskabelonID"}), 404
+
+        ressource_data = [
+            {
+                'RessourceID': ressource.RessourceID,
+                'name': ressource.name,
+                'url': ressource.url
+            } for ressource in ressources
+        ]
+        return jsonify(ressource_data)
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+    finally:
+        session.close()
