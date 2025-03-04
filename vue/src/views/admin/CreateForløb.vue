51fac36e28506d778a61ba80d19c21cb6e1c332d
st-randers <soren.langbo.sloto.thomassen@randers.dk>
2025-03-04 15:20:21 +0100

    Add search to user mails Fix layout for mobile when searching

--- vue/src/views/admin/CreateForløb.vue
+++ vue/src/views/admin/CreateForløb.vue
@@ -1,14 +1,53 @@
 <script setup>
     import { ref } from 'vue'
 
-    const isAdminLocked = ref(true)
+    const inputFields = ref({
+        userMail: "",
+        admin: "",
+        department: "",
+        startDate: "",
+        endDate: "",
+        courseName: ""
+    })
 
+    /* User mail search */
+    const demoUserMailList = ref(["test@randers.dk", "soren.langbo.sloto.thomassen@randers.dk"])
+    const demoUserMailSearchResults = ref([])
+    const isUserMailSearchOpen = ref(false)
+
+    const searchUserMails = (searchString) => {
+        if (isAdminSearchOpen) {
+            isAdminSearchOpen.value = false
+        }
+        if (searchString.length < 3) {
+            isUserMailSearchOpen.value = false
+            return demoUserMailSearchResults.value = []
+        }
+        isUserMailSearchOpen.value = true
+        searchString = searchString.replace(/Æ/gi, 'a').replace(/Ø/gi, 'o').replace(/Å/gi, 'a')
+        let spacelessSearchString = searchString.replace(/ /g, '.')
+        const searchResults = demoUserMailList.value.filter(userMail =>
+            userMail.toLowerCase().startsWith(searchString.toLowerCase()) ||
+            userMail.toLowerCase().startsWith(spacelessSearchString.toLowerCase())
+        )
+        return demoUserMailSearchResults.value = searchResults
+    }
+
+    const selectUserMail = (userMail) => {
+        inputFields.value.userMail = userMail
+        isUserMailSearchOpen.value = false
+    }
+
+    /* Admin search */
+    const isAdminLocked = ref(true)
     const demonAdminList = ref(["Jens Jensen", "Thomas Thomassen"])
     const demoAdminSearchResults = ref([])
-
     const isAdminSearchOpen = ref(false)
 
     const searchAdmins = (searchString) => {
+        if (isUserMailSearchOpen) {
+            isUserMailSearchOpen.value = false
+        }
         if (searchString.length < 3) {
             isAdminSearchOpen.value = false
             return demoAdminSearchResults.value = []
@@ -20,39 +59,42 @@
     }
 
     const selectAdmin = (admin) => {
-        adminRef.value = admin
+        inputFields.value.admin = admin
         isAdminLocked.value = true
         isAdminSearchOpen.value = false
     }
 
     const toggleAdminSearch = () => {
-        if(demonAdminList.value.includes(adminRef.value))
+        if(demonAdminList.value.includes(inputFields.value.admin))
         {
-            
             isAdminLocked.value = !isAdminLocked.value
             isAdminSearchOpen.value = false
         }
         else
-        {
             isAdminLocked.value = false
-        }
     }
 
-    const adminRef = ref("")
 </script>
 
 <template>
     <p class="indent-tiny bold uppercase p-header-adjust">Opret forløb</p>
 
+    <form @submit.prevent="submitForm">
     <div class="formContainer">
 
         <div class="inputContainer">
-            <input type="text" id="name" name="name" placeholder=" " required>
-            <label for="name" class="floating-label">Medarbejder</label>
+            <input type="text" id="name" name="name" placeholder=" " @input="searchUserMails(inputFields.userMail)" v-model="inputFields.userMail" required>
+            <label for="name" class="floating-label">Medarbejder mailadresse</label>
+
+            <div class="itemSelector float-right" v-if="isUserMailSearchOpen">
+                <span class="float-header small uppercase">Vælg en mailadresse ...</span>
+                <div v-for="result in demoUserMailSearchResults" @click="selectUserMail(result)" class="small">{{result}}</div>
+                <div v-if="demoUserMailSearchResults.length == 0" class="nohover small">Der blev ikke fundet nogle resultater.</div>
+            </div>
         </div>
 
-        <div class="inputContainer">
-            <input type="text" id="admin" name="admin" placeholder=" " @input="searchAdmins(adminRef)" v-model="adminRef" class="locked" required :disabled="isAdminLocked">
+        <div :class="['inputContainer', { 'hideOnMobile': isUserMailSearchOpen }]">
+            <input type="text" id="admin" name="admin" placeholder=" " @input="searchAdmins(inputFields.admin)" v-model="inputFields.admin" class="locked" required :disabled="isAdminLocked">
             <label for="admin" class="floating-label">Ansvarlig leder</label>
             <div class="icon" @click="toggleAdminSearch()"><i :class="'fa-solid fa-lock' + (isAdminLocked ? '' : '-open')"></i></div>
             
@@ -63,8 +105,8 @@
             </div>
         </div>
 
-        <div class="inputContainer">
-            <select id="department" name="department" required>
+        <div :class="['inputContainer', { 'hideOnMobile': isUserMailSearchOpen || isAdminSearchOpen }]">
+            <select id="department" name="department" v-model="inputFields.department" required>
                 <option value="" disabled selected hidden></option>
                 <option value="HR">HR</option>
                 <option value="IT">IT</option>
@@ -75,18 +117,29 @@
             <div class="icon nohover"><i class="fa-solid fa-caret-down"></i></div>
         </div>
         
-        <div class="inputContainer">
+        <div :class="['inputContainer', { 'hideOnMobile': isUserMailSearchOpen || isAdminSearchOpen }]">
             <div class="flex-item">
-                <input type="date" id="startDate" name="startDate" required>
+                <input type="date" id="startDate" name="startDate" v-model="inputFields.startDate" required>
                 <label for="startDate" class="floating-label">Startdato</label>
             </div>
             <div class="flex-item">
-                <input type="date" id="startDate" name="startDate" required>
-                <label for="startDate" class="floating-label">Startdato</label>
+                <input type="date" id="endDate" name="endDate" v-model="inputFields.endDate" required>
+                <label for="endDate" class="floating-label">Slutdato</label>
             </div>
         </div>
 
+        <div :class="['inputContainer', { 'hideOnMobile': isUserMailSearchOpen || isAdminSearchOpen  }]">
+            <input type="text" id="name" name="name" placeholder=" " v-model="inputFields.courseName" required>
+            <label for="name" class="floating-label">Forløbets navn</label>
+
+        </div>
+
+        <div class="inputContainer submit">
+            <button class="button button-outline" type="submit">Opret forløb</button>
+        </div>
+
     </div>
+    </form>
 </template>
 
 <style scoped>
@@ -106,10 +159,15 @@
         flex-grow: 1;
         position: relative;
     }
+    .inputContainer.submit {
+        display: flex;
+        flex-direction: row;
+        justify-content: flex-end;
+    }
 
     .float-right {
         position: absolute;
-        margin-top: 4.16rem;
+        margin-top: 5rem; /*4.16rem;*/
         z-index: 10;
         width: 100%;
     }
@@ -118,7 +176,7 @@
             margin-top: 0rem;
             position: absolute;
             margin-left: calc(100% + 1.5rem);
-            max-width: 15rem;
+            max-width: 18rem;
         }
     }
     .itemSelector {
@@ -128,7 +186,12 @@
         border-radius: 0.2rem;
     }
     .itemSelector div {
-        padding: 1rem;
+        padding-left: 1rem;
+        padding-right: 1rem;
+        line-height: 3.15rem;
+
+        overflow: hidden;
+        text-overflow: ellipsis;
     }
     .itemSelector div:first-of-type {
         border-top-left-radius: 0.2rem;
@@ -212,11 +275,19 @@
 
     input[type=text]:focus + .floating-label,
     input[type=text]:not(:placeholder-shown) + .floating-label,
-    input[type=date]:valid + .floating-label,
+    input[type=date] + .floating-label, /* :valid for date, if selection is required */
     select:valid + .floating-label {
         top: 0.5rem;
         left: 0.8rem;
         color: #8b8b8b;
         font-size: 0.8em;
+    }
+
+    @media only screen and (max-width: 768px) {
+        .hideOnMobile {
+            opacity: 0;
+            overflow: hidden;
+            pointer-events: none;
+        }
     }
 </style>
