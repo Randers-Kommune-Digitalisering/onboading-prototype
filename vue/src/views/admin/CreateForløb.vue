755d265ad01cea835a87b5ca301f6ce210eba4e5
st-randers <soren.langbo.sloto.thomassen@randers.dk>
2025-03-05 10:35:06 +0100

    Add backend integration to form

--- vue/src/views/admin/CreateForløb.vue
+++ vue/src/views/admin/CreateForløb.vue
@@ -1,36 +1,46 @@
 <script setup>
-    import { ref } from 'vue'
+    import { ref, onMounted } from 'vue'
 
+    import { getForloebsskabeloner } from '../../services/forløbsskabelonService';
+    import { createForloeb } from '../../services/forløbService';
+    import keycloak from '@/keycloak';
+    import { getAdminNames, getEmail, getDQ } from '../../services/userService';
+
+    const templates = ref([])
     const inputFields = ref({
-        userMail: "",
+        usermail: "",
         admin: "",
-        department: "",
-        startDate: "",
-        endDate: "",
-        courseName: ""
+        ForløbsskabelonID: "",
+        startdate: "",
+        enddate: "",
+        name: "",
+        privateEmail: '',
+        userdq: ''
     })
 
+    // const dqList = ref([])
+
     /* User mail search */
-    const demoUserMailList = ref(["test@randers.dk", "soren.langbo.sloto.thomassen@randers.dk"])
-    const demoUserMailSearchResults = ref([])
+    const userMailList = ref([])
+    const userMailSearchResults = ref([])
     const isUserMailSearchOpen = ref(false)
 
     const searchUserMails = (searchString) => {
-        if (isAdminSearchOpen) {
-            isAdminSearchOpen.value = false
+        if (isadminSearchOpen) {
+            isadminSearchOpen.value = false
         }
         if (searchString.length < 3) {
             isUserMailSearchOpen.value = false
-            return demoUserMailSearchResults.value = []
+            return userMailSearchResults.value = []
         }
         isUserMailSearchOpen.value = true
         searchString = searchString.replace(/Æ/gi, 'a').replace(/Ø/gi, 'o').replace(/Å/gi, 'a')
         let spacelessSearchString = searchString.replace(/ /g, '.')
-        const searchResults = demoUserMailList.value.filter(userMail =>
+        const searchResults = userMailList.value.filter(userMail =>
             userMail.toLowerCase().startsWith(searchString.toLowerCase()) ||
             userMail.toLowerCase().startsWith(spacelessSearchString.toLowerCase())
         )
-        return demoUserMailSearchResults.value = searchResults
+        return userMailSearchResults.value = searchResults
     }
 
     const selectUserMail = (userMail) => {
@@ -38,41 +48,109 @@
         isUserMailSearchOpen.value = false
     }
 
+    const getEmailType = () => {
+        if (inputFields.value.usermail.includes('@randers.dk')) {
+            return 'randersmail'
+        }
+        return 'private'
+    }
+
     /* Admin search */
     const isAdminLocked = ref(true)
-    const demonAdminList = ref(["Jens Jensen", "Thomas Thomassen"])
-    const demoAdminSearchResults = ref([])
-    const isAdminSearchOpen = ref(false)
+    const adminList = ref([])
+    const adminSearchResults = ref([])
+    const isadminSearchOpen = ref(false)
 
     const searchAdmins = (searchString) => {
         if (isUserMailSearchOpen) {
             isUserMailSearchOpen.value = false
         }
         if (searchString.length < 3) {
-            isAdminSearchOpen.value = false
-            return demoAdminSearchResults.value = []
+            isadminSearchOpen.value = false
+            return adminSearchResults.value = []
         }
-        isAdminSearchOpen.value = true
-        return demoAdminSearchResults.value = demonAdminList.value.filter(admin =>
-            admin.toLowerCase().includes(searchString.toLowerCase())
-        )
+        isadminSearchOpen.value = true
+        return adminSearchResults.value = adminList.value
+            .filter(admin => admin.toLowerCase().includes(searchString.toLowerCase()))
+            .slice(0, 8)
     }
 
     const selectAdmin = (admin) => {
         inputFields.value.admin = admin
         isAdminLocked.value = true
-        isAdminSearchOpen.value = false
+        isadminSearchOpen.value = false
     }
 
-    const toggleAdminSearch = () => {
-        if(demonAdminList.value.includes(inputFields.value.admin))
+    const toggleadminSearch = () => {
+        if(adminList.value.includes(inputFields.value.admin))
         {
             isAdminLocked.value = !isAdminLocked.value
-            isAdminSearchOpen.value = false
+            isadminSearchOpen.value = false
         }
         else
             isAdminLocked.value = false
     }
+
+    const clearAdminIfNotSelected = () => {
+        if(!adminList.value.includes(inputFields.value.admin))
+        {
+            inputFields.value.admin = ""
+            isAdminLocked.value = false
+            isadminSearchOpen.value = false
+        }
+    }
+
+    /* Instantiate */
+    onMounted(() => {
+        getForloebsskabeloner().then(data => {
+            templates.value = data.data
+        }).catch(error => {
+            console.error('Error fetching forloebsskabeloner:', error)
+        })
+
+        getAdminNames().then(data => {
+            adminList.value = data.data.admin_names
+        }).catch(error => {
+            console.error('Error fetching admin names:', error)
+        })
+
+        getEmail().then(data => {
+            userMailList.value = data.data.emails
+        }).catch(error => {
+            console.error('Error fetching emails:', error)
+        })
+
+        // getDQ().then(data => {
+        //     dqList.value = data.data.dq_numbers
+        //     console.log('DQs:', data.data)
+        // }).catch(error => {
+        //     console.error('Error fetching DQs:', error)
+        // })
+    })
+
+    /* Submit */
+
+    const submitForm = async () =>
+    {
+        try {
+            const formData = { ...inputFields.value }
+            if (!formData.ForløbsskabelonID)
+                delete formData.ForløbsskabelonID
+
+            if (getEmailType() === 'private')
+            {
+                formData.privateEmail = formData.usermail
+                delete formData.usermail
+            }
+            else 
+                delete formData.privateEmail
+
+            const response = await createForloeb(formData)
+            console.log('Response:', response.data.message)
+        } catch (error) {
+            console.log('Error:', response.data.error)
+        }
+}
 
 </script>
 
@@ -83,59 +161,57 @@
     <div class="formContainer">
 
         <div class="inputContainer">
-            <input type="text" id="name" name="name" placeholder=" " @input="searchUserMails(inputFields.userMail)" v-model="inputFields.userMail" required>
-            <label for="name" class="floating-label">Medarbejder mailadresse</label>
+            <input type="text" id="mail" name="mail" placeholder=" " @input="searchUserMails(inputFields.usermail)" v-model="inputFields.usermail" required>
+            <label for="mail" class="floating-label">Medarbejder mailadresse</label>
 
             <div class="itemSelector float-right" v-if="isUserMailSearchOpen">
                 <span class="float-header small uppercase">Vælg en mailadresse ...</span>
-                <div v-for="result in demoUserMailSearchResults" @click="selectUserMail(result)" class="small">{{result}}</div>
-                <div v-if="demoUserMailSearchResults.length == 0" class="nohover small">Der blev ikke fundet nogle resultater.</div>
+                <div v-for="result in userMailSearchResults" @click="selectUserMail(result)" class="small">{{result}}</div>
+                <div v-if="userMailSearchResults.length == 0" class="nohover small">Der blev ikke fundet nogle resultater.</div>
             </div>
         </div>
 
         <div :class="['inputContainer', { 'hideOnMobile': isUserMailSearchOpen }]">
             <input type="text" id="admin" name="admin" placeholder=" " @input="searchAdmins(inputFields.admin)" v-model="inputFields.admin" class="locked" required :disabled="isAdminLocked">
             <label for="admin" class="floating-label">Ansvarlig leder</label>
-            <div class="icon" @click="toggleAdminSearch()"><i :class="'fa-solid fa-lock' + (isAdminLocked ? '' : '-open')"></i></div>
+            <div class="icon" @click="toggleadminSearch()"><i :class="'fa-solid fa-lock' + (isAdminLocked ? '' : '-open')"></i></div>
             
-            <div class="itemSelector float-right" v-if="isAdminSearchOpen">
+            <div class="itemSelector float-right" v-if="isadminSearchOpen">
                 <span class="float-header small uppercase">Vælg en ansvarlig leder ...</span>
-                <div v-for="result in demoAdminSearchResults" @click="selectAdmin(result)">{{result}}</div>
-                <div v-if="demoAdminSearchResults.length == 0" class="nohover small">Der blev ikke fundet nogle resultater.</div>
+                <div v-for="result in adminSearchResults" @click="selectAdmin(result)">{{result}}</div>
+                <div v-if="adminSearchResults.length == 0" class="nohover small">Der blev ikke fundet nogle resultater.</div>
             </div>
         </div>
 
-        <div :class="['inputContainer', { 'hideOnMobile': isUserMailSearchOpen || isAdminSearchOpen }]">
-            <select id="department" name="department" v-model="inputFields.department" required>
-                <option value="" disabled selected hidden></option>
-                <option value="HR">HR</option>
-                <option value="IT">IT</option>
-                <option value="Finance">Finance</option>
-                <option value="Marketing">Marketing</option>
+        <div :class="['inputContainer', { 'hideOnMobile': isUserMailSearchOpen || isadminSearchOpen }]">
+            <select id="template" name="template" v-model="inputFields.ForløbsskabelonID">
+                <option :value="null" disabled selected hidden></option>
+                <option v-for="template in templates" :value="template.ForløbsskabelonID">{{template.name}}</option>
+                <option v-if="templates.length == 0" disabled>Ingen skabeloner</option>
             </select>
-            <label for="department" class="floating-label">Afdeling</label>
+            <label for="template" class="floating-label">Skabelon</label>
             <div class="icon nohover"><i class="fa-solid fa-caret-down"></i></div>
         </div>
         
-        <div :class="['inputContainer', { 'hideOnMobile': isUserMailSearchOpen || isAdminSearchOpen }]">
+        <div :class="['inputContainer', { 'hideOnMobile': isUserMailSearchOpen || isadminSearchOpen }]">
             <div class="flex-item">
-                <input type="date" id="startDate" name="startDate" v-model="inputFields.startDate" required>
-                <label for="startDate" class="floating-label">Startdato</label>
+                <input type="date" id="startdate" name="startdate" v-model="inputFields.startdate" required>
+                <label for="startdate" class="floating-label">Startdato</label>
             </div>
             <div class="flex-item">
-                <input type="date" id="endDate" name="endDate" v-model="inputFields.endDate" required>
-                <label for="endDate" class="floating-label">Slutdato</label>
+                <input type="date" id="enddate" name="enddate" v-model="inputFields.enddate" required>
+                <label for="enddate" class="floating-label">Slutdato</label>
             </div>
         </div>
 
-        <div :class="['inputContainer', { 'hideOnMobile': isUserMailSearchOpen || isAdminSearchOpen  }]">
+        <div :class="['inputContainer', { 'hideOnMobile': isUserMailSearchOpen || isadminSearchOpen  }]">
             <input type="text" id="name" name="name" placeholder=" " v-model="inputFields.courseName" required>
             <label for="name" class="floating-label">Forløbets navn</label>
 
         </div>
 
         <div class="inputContainer submit">
-            <button class="button button-outline" type="submit">Opret forløb</button>
+            <button class="button button-outline" @click="clearAdminIfNotSelected()" type="submit">Opret forløb</button>
         </div>
 
     </div>
