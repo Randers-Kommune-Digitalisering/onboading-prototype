548a53cef4f7d8207fa0fe60112cbfba610ecbe8
st-randers <soren.langbo.sloto.thomassen@randers.dk>
2025-03-11 14:50:50 +0100

    Separate ongoing and completed forløb + tasks

--- vue/src/views/admin/AdminOverview.vue
+++ vue/src/views/admin/AdminOverview.vue
@@ -2,14 +2,11 @@
 	import { ref, onMounted } from 'vue'
 	import keycloak from '@/keycloak'
 
-	import { getAllForloeb, getForloebByAdmin } from '../../services/forløbService'
-	import { getOpgaverByForloebID } from '../../services/opgaveService'
+	import { getForloebByAdmin } from '@/services/forløbService'
 	import CourseList from '@/components/CourseList.vue'
 
-	const ongoingForloeb = ref([])
-	const completedForloeb = ref([])
-
-	const testData = [{'id': 1, 'title': 'Demo forløb 1', 'name': 'Soren T'},{'id': 1, 'title': 'Demo forløb 1', 'name': 'Soren T'}]
+	const forloeb_ongoing = ref([])
+	const forloeb_completed = ref([])
 
 	onMounted(async () => {
 		if (keycloak.authenticated) {
@@ -22,11 +19,28 @@
 
 			const headers =  { adminname: loggedInAdmin }
 			const response = await getForloebByAdmin({headers})
-			ongoingForloeb.value = response.data
+
+			if (response.data == null)
+				return
+
+			if (!Array.isArray(response.data))
+				response.data = [response.data]
+
+			for (const item of response.data) {
+				if (item.enddate < new Date())
+					forloeb_completed.value.push(item)
+				else 
+					forloeb_ongoing.value.push(item)
+			}
+
+			// Sort and limit the number of completed courses
+			forloeb_completed.value.sort((a, b) => new Date(b.enddate) - new Date(a.enddate))
+			forloeb_completed.value = forloeb_completed.value.slice(0, 15)
 		}
 	})
 </script>
 
 <template>
-  <CourseList :courses="ongoingForloeb" />
+  <CourseList :courses="forloeb_ongoing" />
+  <CourseList :courses="forloeb_completed" title="Afsluttede forløb" />
 </template>
