23d5973d7c77d3368505e9a09bf22d15dc83274b
st-randers <soren.langbo.sloto.thomassen@randers.dk>
2025-03-14 14:44:21 +0100

    Fix task colors

--- vue/src/components/TaskList.vue
+++ vue/src/components/TaskList.vue
@@ -60,7 +60,7 @@
             :responsible="task.ansvarlig"
             :booking="booking ? new Date(new Date(task.booking)) : null"
             image="https://www.teknologisk.dk/_/media/67761&w=1460&h=808&r=cover&_filename=67761_7-gode-rr%C3%A5d-til-IT-sikkerhed.jpg"
-            :color="props.itemColor != null ? props.itemColor : new Date(new Date(task.slutdato).getTime() + 8 * 60 * 60 * 1000) < new Date() ? 'bf4e4e' : defaultItemColor"
+            :color="props.itemColor != null ? props.itemColor : (!templateView && new Date(task.slutdato) < new Date()) ? 'bf4e4e' : defaultItemColor"
             isComplete="false"
             :expandByDefault="expandFirstItem && index == 0"
             :dark="dark"
