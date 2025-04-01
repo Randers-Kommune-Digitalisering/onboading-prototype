cf8b0d77f8706f4addb90d79ba568114e4eb1978
Rune Aagaard Keena <rune.aagaard.keena@randers.dk>
2025-04-01 09:08:48 +0200

    remove keycloak from healthz and metrics endpoints

--- python/src/main.py
+++ python/src/main.py
@@ -31,7 +31,7 @@ def create_app():
 
         @app.before_request
         def check_authenticated():
-            if 'user' not in session and request.path != '/login' and request.path != '/auth':
+            if 'user' not in session and request.path not in ['/login', '/auth', '/healthz', '/metrics']:
                 return redirect(url_for("login"))
 
         @app.route("/login")
